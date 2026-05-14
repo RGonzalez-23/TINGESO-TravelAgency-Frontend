9464962d0addcd884c0e74561a7fd51ae0bce9cf
Raul <raul.gonzalez.v@usach.cl>
2026-05-13 20:59:55 -0400

    feat: update docker-compose for frontend deployment and refine API base URL configuration in http-common

--- src/http-common.js
+++ src/http-common.js
@@ -1,13 +1,13 @@
 import axios from "axios";
 import keycloak from "./services/keycloak";
 
-const travelBackendServer = import.meta.env.VITE_TRAVEL_BACKEND_SERVER;
-const travelBackendPort = import.meta.env.VITE_TRAVEL_BACKEND_PORT;
-const apiHost = travelBackendServer || "localhost";
-const apiPort = travelBackendPort || "8090";
+const apiHost = import.meta.env.VITE_TRAVEL_BACKEND_SERVER;
+const apiPort = import.meta.env.VITE_TRAVEL_BACKEND_PORT;
 
+// Production (Docker with Nginx), we  use relative routes for the proxy.
+// If local development (Vite), we use the .env variables.
 const api = axios.create({
-  baseURL: `http://${apiHost}:${apiPort}`,
+  baseURL: (apiHost && apiPort) ? `http://${apiHost}:${apiPort}` : "",
   headers: {
     "Content-Type": "application/json"
   }
